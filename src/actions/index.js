c412eb0e23873217b33400e154d57841cc7864b2
Coleen Huang <coleenhuang@gmail.com>
2020-08-05 09:43:53 -0700

    finished project

---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+import _ from 'lodash'
+
+export const fetchPostsandUsers = () => async (dispatch, getState) => {
+  console.log('About to fetch posts')
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
+export const fetchPosts = () => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    })
+}}
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: 'FETCH_USER',
+     payload: response.data
+   })
+}}
